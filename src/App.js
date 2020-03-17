8edd1dd599b77d8d1c0bc3869a5e431674ef5faf
Maya Estalilla <mayaestalilla@gmail.com>
2020-03-17 12:59:06 -0400

    Resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react'
-import ListContacts from './ListContacts'
-import * as ContactsAPI from './utils/ContactsAPI'
-import CreateContact from './CreateContact'
-import { Route } from 'react-router-dom'
-
-class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll()
-      .then((contacts) => {
-        this.setState(() => ({
-          contacts
-        }))
-      })
-  }
-  removeContact = (contact) => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter((c) => {
-        return c.id !== contact.id
-      })
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-  createContact = (contact) => {
-    ContactsAPI.create(contact)
-      .then((contact) => {
-        this.setState((currentState) => ({
-          contacts: currentState.contacts.concat([contact])
-        }))
-      })
-  }
-  render() {
-    return (
-      <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-          />
-        )} />
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )} />
-=======
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
@@ -94,10 +40,9 @@ class App extends Component {
           contacts={this.state.contacts}
           onDeleteContact={this.removeContact}
         />
->>>>>>> starter-files-added
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
